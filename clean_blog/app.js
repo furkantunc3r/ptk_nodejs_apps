84953d6fc115fa47c6ed0132c698643e250c205c
furkantunc3r <furkan_tuncer@yahoo.com>
2024-12-01 20:14:18 +0300

    Update

--- clean_blog/app.js
+++ clean_blog/app.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    const blog = {
-        id: 1,
-        title: 'Blog title',
-        description: 'Blog description'
-    };
 
-    res.status(200).send(blog);
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARE
+app.use(express.static('public'));
+
+// ENDPOINTS
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/add', (req, res) => {
+    res.render('add');
+});
+
+// LISTENER
 app.listen(3000, () => {
     console.log(`App listening on: ${ port }`);
 });
