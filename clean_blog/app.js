5f6a0fc11525e61ec1add6fba04ae66cda8ea0d7
furkantunc3r <furkan_tuncer@yahoo.com>
2024-12-06 01:10:26 +0300

    deploy

--- clean_blog/app.js
+++ clean_blog/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+require('dotenv').config();
 
 const ejs = require('ejs');
 const path = require('path');
